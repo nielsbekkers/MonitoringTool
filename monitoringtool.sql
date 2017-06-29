e8397dad11f8937b09894c8c36c23e534195f87c
Frederik <frederik.vanstraelen@student.ucll.be>
2017-06-29 15:29:08 +0200

    sql file update with sales data

--- monitoringtool.sql
+++ monitoringtool.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 29 jun 2017 om 14:46
+-- Gegenereerd op: 29 jun 2017 om 15:28
 -- Serverversie: 10.1.16-MariaDB
 -- PHP-versie: 7.0.9
 
@@ -81,6 +81,20 @@ CREATE TABLE `product_types` (
   `Amount` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `sales`
+--
+
+DROP TABLE IF EXISTS `sales`;
+CREATE TABLE `sales` (
+  `Id` int(11) NOT NULL,
+  `PId` int(11) NOT NULL,
+  `Serial_Number` varchar(50) NOT NULL,
+  `Client_Id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 --
 -- Indexen voor geëxporteerde tabellen
 --
@@ -97,7 +111,8 @@ ALTER TABLE `database_changed`
 ALTER TABLE `product_detail`
   ADD PRIMARY KEY (`Id`),
   ADD UNIQUE KEY `Serial_Number` (`Serial_Number`),
-  ADD KEY `PId` (`PId`);
+  ADD KEY `PId` (`PId`),
+  ADD KEY `Serial_Number_2` (`Serial_Number`);
 
 --
 -- Indexen voor tabel `product_detail_last_25`
@@ -113,6 +128,16 @@ ALTER TABLE `product_detail_last_25`
 ALTER TABLE `product_types`
   ADD PRIMARY KEY (`PId`),
   ADD UNIQUE KEY `Name` (`Name`);
+
+--
+-- Indexen voor tabel `sales`
+--
+ALTER TABLE `sales`
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `Serial_Number` (`Serial_Number`),
+  ADD KEY `Serial_Number_2` (`Serial_Number`),
+  ADD KEY `PId` (`PId`),
+  ADD KEY `Client_Id` (`Client_Id`);
 
 --
 -- AUTO_INCREMENT voor geëxporteerde tabellen
@@ -134,6 +159,11 @@ ALTER TABLE `product_detail`
 ALTER TABLE `product_detail_last_25`
   MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT voor een tabel `sales`
+--
+ALTER TABLE `sales`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- Beperkingen voor geëxporteerde tabellen
 --
 
@@ -148,6 +178,13 @@ ALTER TABLE `product_detail`
 --
 ALTER TABLE `product_detail_last_25`
   ADD CONSTRAINT `product_detail_last_25_ibfk_1` FOREIGN KEY (`PId`) REFERENCES `product_types` (`PId`);
+
+--
+-- Beperkingen voor tabel `sales`
+--
+ALTER TABLE `sales`
+  ADD CONSTRAINT `sales_ibfk_1` FOREIGN KEY (`PId`) REFERENCES `product_types` (`PId`),
+  ADD CONSTRAINT `sales_ibfk_2` FOREIGN KEY (`Serial_Number`) REFERENCES `product_detail` (`Serial_Number`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
